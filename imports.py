9e9a52f672bd43db43f5b9c205b093e84aae9a0a
Samjus02 <samuel.jusic@hotmail.se>
2025-12-10 08:53:20 +0100

    Initial commit

---
+++ imports.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import math
+
+from scipy import optimize
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.linear_model import LogisticRegression, LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import (
+    mean_squared_error,
+    mean_absolute_error,
+    r2_score,
+    confusion_matrix,
+)
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
